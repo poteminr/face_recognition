6e663f946c4e44b9e676ff9d994a12fb6c09d6aa
poteminr <poteminr@gmail.com>
2019-02-20 08:09:11 -0500

    add validate func

--- preproc.py
+++ preproc.py
@@ -1,5 +1,23 @@
 import cv2
 import numpy as np
+
+from keras.models import model_from_json
+
+json_decoder = ''
+json_encoder = ''
+
+with open('encoder_mri_json.txt', 'r') as file:
+    json_encoder = file.readline()
+
+with open('decoder_mri_json.txt', 'r') as file:
+    json_decoder = file.readline()
+
+D = model_from_json(json_decoder)
+D.load_weights('decoder_weights_mri.h5')
+
+E = model_from_json(json_encoder)
+E.load_weights('encoder_weights_mri.h5')
+
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
@@ -18,7 +36,6 @@ def crop_faces(image):
     face_crop = []
     for f in faces:
         x, y, w, h = [v for v in f]
-        #  cv2.rectangle(image_copy, (x,y), (x+w, y+h), (255,0,0), 3)
         face_crop.append(gray_image[y:y + h, x:x + w])
 
     return face_crop
@@ -45,3 +62,91 @@ def distance(emb1, emb2):
     :return: Расстояние между векторами
     """
     return np.sum(np.square(emb1 - emb2))
+
+
+def face_embedding(image):
+    """
+    :param image: Изображение, открытое через cv2.imread()
+    :return: Возвращает array с embedding`ом лиц, если в image лиц >1
+             Если лицо одно, то возвращает array с embedding лица
+    """
+    face_list = crop_faces(image)
+
+    if len(face_list) == 1:
+        face = face_list[0]
+        res_face = cv2.resize(face, (64, 64))
+
+        res_face = np.expand_dims(res_face, 0)
+        res_face = np.expand_dims(res_face, -1)
+        res_face = res_face / 255.
+        face_encoded = E.predict(res_face)
+        return np.array(face_encoded)
+
+    elif len(face_list) > 1:
+        faces_encoded = []
+        for face in face_list:
+            res_face = cv2.resize(face, (64, 64))
+
+            res_face = np.expand_dims(res_face, 0)
+            res_face = np.expand_dims(res_face, -1)
+            res_face = res_face / 255.
+            face_vec = E.predict(res_face)
+
+            faces_encoded.append(np.array(face_vec))
+        return np.array(faces_encoded)
+
+
+class face_values_error(Exception):
+
+    pass
+
+
+def face_distance(image1,image2):
+    """
+
+    :param image1: Изображение с одним лицом.
+    :param image2: Изображение с одним лицом, если их несколько, падает Error
+    :return: Вернет дистанцию между embedding`ами
+    """
+    image1_vec = face_embedding(image1)
+    image2_vec = face_embedding(image2)
+    if len(image1_vec) > 1 or len(image2_vec) >1:
+        raise face_values_error('Face in image > 1')
+    else:
+        return distance(image1_vec,image2_vec)
+
+
+
+
+def valid_embedding(image,model):
+    """
+
+    :param image: Изображение через cv2.imread
+    :param model: Обученный энкодер для валидации
+    :return: Вернет array с вектором,если лицо одно.
+            Вернет array с array`ями вектором, если больше одного.
+    """
+    face_list = crop_faces(image)
+
+    if len(face_list) == 1:
+        face = face_list[0]
+        res_face = cv2.resize(face, (64, 64))
+
+        res_face = np.expand_dims(res_face, 0)
+        res_face = np.expand_dims(res_face, -1)
+        res_face = res_face / 255.
+        face_encoded = model.predict(res_face)
+        return np.array(face_encoded)
+
+    elif len(face_list) > 1:
+        faces_encoded = []
+        for face in face_list:
+            res_face = cv2.resize(face, (64, 64))
+
+            res_face = np.expand_dims(res_face, 0)
+            res_face = np.expand_dims(res_face, -1)
+            res_face = res_face / 255.
+            face_vec = model.predict(res_face)
+
+            faces_encoded.append(np.array(face_vec))
+        return np.array(faces_encoded)
